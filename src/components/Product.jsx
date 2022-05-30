0f0bf449643e51a7f823e23706f1fcc4936b5166
SachinKumar0381 <sachinkuom0591998@gmail.com>
2022-05-30 16:07:53 +0530

    first

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,24 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Product = () => {
+const Product = (props) => {
   // TODO: Remove below const and instead import them from chakra
-  const Text = () => <div />;
-  const Image = () => <div />;
-  const Box = () => <div />;
-  const Stack = () => <div />;
-  const Heading = () => <div />;
-  const Tag = () => <div />;
-  const TagLabel = () => <div />;
+ const [item,setItem]=useState(props.title);
+ setItem(props.title);
   return (
-    <Stack data-cy="product">
-      <Image data-cy="product-image" />
-      <Text data-cy="product-category"></Text>
-      <Tag>
-        <TagLabel data-cy="product-gender"></TagLabel>
-      </Tag>
-      <Heading data-cy="product-title"></Heading>
-      <Box data-cy="product-price"></Box>
-    </Stack>
+    // <Stack data-cy="product">
+    //   <Image data-cy="product-image" />
+    //   <Text data-cy="product-category"></Text>
+    //   <Tag>
+    //     <TagLabel data-cy="product-gender"></TagLabel>
+    //   </Tag>
+    //   <Heading data-cy="product-title"></Heading>
+    //   <Box data-cy="product-price"></Box>
+      
+    // </Stack>
+    <>
+    <h1>{item}</h1>
+    </>
   );
 };
 
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,16 +1,26 @@
-import React from "react";
-
+import React, { useEffect, useState } from "react";
+import Product from "./Product";
 const Products = () => {
   // TODO: Remove below const and instead import them from chakra
-  const Flex = () => <div />;
-  const Grid = () => <div />;
-
+  const [items,setItems]=useState([]);
+  useEffect(()=>{
+    const data=()=>{
+      fetch('http://localhost:8080/products')
+      .then((r)=>r.json())
+      .then((r)=>{
+        setItems(r);
+      })
+    }
+    data();
+  },[]);
+  
+  
   return (
-    <Flex>
-      {/*  AddProduct */}
-      <Grid>{/* List of Products */}</Grid>
-      {/* Pagination */}
-    </Flex>
+    <div>
+        <h1>products</h1>
+           <Product {...items}/>
+        
+    </div>
   );
 };
 
